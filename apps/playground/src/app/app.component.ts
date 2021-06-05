edbfc4a16fe7754e8712d885f33d8dcfbc9ee2d5
markostanimirovic <markostanimirovic95@gmail.com>
2021-06-05 17:15:50 +0200

    chore: change npmScope and prefix to rx-mind

--- apps/playground/src/app/app.component.ts
+++ apps/playground/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ngrx-plugins-root',
+  selector: 'rx-mind-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
